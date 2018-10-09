dcaf661c4dfd9e5c7707a5320b11e7ed009567ae
U Akshay Bharadwaj <akshay.b@quadwave.lan>
2018-10-09 18:01:58 +0530

    Changes For SW

--- Engine/sw.js
+++ Engine/sw.js
@@ -13,6 +13,7 @@ self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function (cache) {
+                debugger;
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
